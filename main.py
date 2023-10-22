e3fb4e9580557da5c08352c5ff80dbef21f43950
ArasChai <araschaigne@gmail.com>
2023-10-22 18:42:12 +0200

    [TEST] changed all the main code to what works

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-from dscrd import starte
-from server import run
-import concurrent.futures
+# from dscrd import starte
+# from server import run
+# import concurrent.futures
 
 
 
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
 
-    run()
+#     run()
+
+from flask import Flask, jsonify
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
