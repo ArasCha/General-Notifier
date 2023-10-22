a23c7f2682e40b6da58e6fe6830488662e9ba3b3
ArasChai <araschaigne@gmail.com>
2023-10-22 15:42:32 +0200

    [EDIT] railway.json added command to launch the python program

--- dscrd.py
+++ dscrd.py
@@ -11,7 +11,8 @@ client = commands.Bot(command_prefix = '!')
 
 running = True
 
-notification_channel = int(os.environ["DISCORD_CHANNEL"])
+config = dotenv_values(".env")  
+notification_channel = int(config["DISCORD_CHANNEL"])
 
 
 #-----------------------------------------------------------------------------------------
@@ -126,4 +127,4 @@ def set_running(val:bool) -> None:
 
 
 def starte():
-    client.run(os.environ["DISCORD_BOT_TOKEN"])
+    client.run(config["DISCORD_BOT_TOKEN"])
