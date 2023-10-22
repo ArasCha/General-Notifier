0d9b1539d28b49b0d07e3df2138a819da2945417
ArasChai <araschaigne@gmail.com>
2023-10-22 19:09:46 +0200

    [ADD] restored server.py

---
+++ server.py
@@ -0,0 +1,41 @@
+import json
+from dotenv import dotenv_values
+from flask import Flask, request
+import asyncio
+import os
+
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["POST"])
+def handle_post_request():
+
+    try:
+        print("a request is received")
+        return "si"
+        content = request.get_data().decode("utf-8")
+        data = json.loads(content)
+        if data["authorization"] != dotenv_values(".env")["AUTHORIZATION_TOKEN"]:
+            return 'Bad authorization token'
+        
+        try:
+            return "oui"
+
+        except Exception as e:
+            print(e)
+            return "Server error. The Discord bot might not be launched"
+
+    except Exception as e:
+        return 'Send JSON data in the format {"message":"...", "authorization":"..."}:' + f"\n{e}"
+
+    return "OK"
+
+
+def run():
+
+    env = dotenv_values(".env")
+    # HOST = env["LISTENER_HOST"]
+    PORT = int(env["LISTENER_PORT"])
+
+    app.run(port=os.getenv("PORT", default=5000))
