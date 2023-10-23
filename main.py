ebd0b44229dfd58a5191d53c95d4dc6086f7d81e
ArasChai <araschaigne@gmail.com>
2023-10-23 11:04:50 +0200

    [TEST] if without get method it works

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
 from flask import Flask, request
 import os
-from dotenv import dotenv_values
 import json
 import traceback
 
 
 app = Flask(__name__)
-
-
-@app.route('/')
-def index():
-    return '{"Choo Choo": "Welcome to your Flask app 🚅"}'
 
 
 @app.route("/", methods=["POST"])
@@ -19,9 +13,6 @@ def handle_post_request():
     try:
         content = request.get_data().decode("utf-8")
         data = json.loads(content)
-        print("data:", data)
-        print(os.getenv("AUTHORIZATION_TOKEN"))
-        print(dotenv_values(".env"))
         if data["authorization"] != os.getenv("AUTHORIZATION_TOKEN"):
             return 'Bad authorization token'
         
@@ -37,8 +28,6 @@ def handle_post_request():
         return f'''Send JSON data in the format {"message":"...", "authorization":"..."}:
             {traceback.format_exc()}
         '''
-
-    return "OK"
 
 
 if __name__ == '__main__':
