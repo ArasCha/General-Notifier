156e78bb460eff7ae291ee54871070dae2fbf40b
ArasChai <araschaigne@gmail.com>
2023-10-25 12:45:13 +0200

    [TEST FIX] client start in thread and not flask app

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ from server import App, options, app
 import concurrent.futures
 import os
 import asyncio
-
+from threading import Thread
 
 
 
 if __name__ == "__main__":
 
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        futures = [App(app, options).run(), asyncio.run(client.start(os.getenv("DISCORD_BOT_TOKEN")))]
+    loop = asyncio.get_event_loop()
+    loop.create_task(client.start(os.getenv("DISCORD_BOT_TOKEN")))
+    Thread(target=loop.run_forever).start()
+
+    App(app, options).run()
         
