564426aadba72d2a716b2dafcadf9373939c17be
ArasCha <araschaigne@gmail.com>
2026-01-27 16:35:34 +0100

    [EDIT] gets json instead of data [FIX] returns status code relevant to behavior

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
-import json
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import asyncio
 from bot import notifier, client, env
 import traceback
@@ -11,24 +10,37 @@ app = Flask(__name__)
 
 @app.route("/", methods=["POST"])
 def handle_post_request():
-
+    """
+    Accepts requests in such format:
+    POST
+    url = http://{env["LISTENER_HOST"]}:{env["LISTENER_PORT"]}
+    headers = { "Accept-Charset": "utf-8" }
+    payload = {"authorization": <authorization>, "message": <message>}
+    requests.post(url, headers, json=payload)
+    """
     try:
-        content = request.get_data().decode("utf-8")
-        data = json.loads(content)
+        data = request.get_json()  # or force=False and handle None
+
+        if not data:
+            return jsonify(error="Invalid or missing JSON"), 400
+
+        if "authorization" not in data or "message" not in data:
+            return jsonify(error="Missing 'authorization' or 'message'"), 400
+
         if data["authorization"] != env["AUTHORIZATION_TOKEN"]:
-            return 'Bad authorization token'
+            return jsonify(error="Bad authorization token"), 401
         
         try:
             asyncio.run_coroutine_threadsafe(notifier(data["message"]), client.loop)
             return "OK"
 
         except:
-            return f"""Server error. Error executing the notifier:
-                {traceback.format_exc()}"""
+            return jsonify(error=f"""Server error. Error executing the notifier:
+                {traceback.format_exc()}"""), 500
 
     except:
-        return f"""Server error:
-            {traceback.format_exc()}"""
+        return jsonify(error=f"""Server error. Error executing the notifier:
+                {traceback.format_exc()}"""), 500
 
 
 
