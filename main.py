18e06e1a37e05d4047c2edbcc3192f29ff347d77
ArasCha <araschaigne@gmail.com>
2026-05-26 16:58:05 +0200

    [ADD] ability to retry if Network connection error

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from bot import client, env
 from server import run as run_server
 import concurrent.futures
+import time
+import aiohttp, socket
 
 
+def main():
+
+    while True:
+
+        try:
+
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                executor.submit(run_server)
+                client.run(env["DISCORD_BOT_TOKEN"])
+            
+        except (aiohttp.ClientError, socket.gaierror, OSError) as exc:
+            print(f"Network unavailable: {exc}. Retrying in 30 seconds...")
+            time.sleep(INTERNET_CONNECTION_RETRY)
 
 
 if __name__ == "__main__":
 
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        futures = [executor.submit(run_server), client.run(env["DISCORD_BOT_TOKEN"])]
+    INTERNET_CONNECTION_RETRY = 600 # seconds
+    main()
